8e9a2a18d87242f5690745f27c8ef0329c936a98
JoeRscL <jonathan20strada@gmail.com>
2025-10-03 11:22:04 +0700

    Commit No.4 - Assigment

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -27,38 +27,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerView.adapter = catAdapter
-        recyclerView.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.VERTICAL,
-            false
-        )
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         val itemTouchHelper = ItemTouchHelper(catAdapter.swipeToDeleteCallback)
         itemTouchHelper.attachToRecyclerView(recyclerView)
 
         catAdapter.setData(
             listOf(
-                CatModel(
-                    Gender.Male,
-                    CatBreed.BalineseJavanese,
-                    "Fred",
-                    "Silent and deadly",
-                    "https://cdn2.thecatapi.com/images/7dj.jpg"
-                ),
-                CatModel(
-                    Gender.Female,
-                    CatBreed.ExoticShorthair,
-                    "Wilma",
-                    "Cuddly assassin",
-                    "https://cdn2.thecatapi.com/images/egv.jpg"
-                ),
-                CatModel(
-                    Gender.Unknown,
-                    CatBreed.AmericanCurl,
-                    "Curious George",
-                    "Award winning investigator",
-                    "https://cdn2.thecatapi.com/images/bar.jpg"
-                )
+                CatModel(Gender.Male, CatBreed.BalineseJavanese, "Fred", "Silent and deadly", "https://cdn2.thecatapi.com/images/7dj.jpg"),
+                CatModel(Gender.Female, CatBreed.ExoticShorthair, "Wilma", "Cuddly assassin", "https://cdn2.thecatapi.com/images/egv.jpg"),
+                CatModel(Gender.Unknown, CatBreed.AmericanCurl, "Curious George", "Award winning investigator", "https://cdn2.thecatapi.com/images/bar.jpg"),
+                CatModel(Gender.Male, CatBreed.MaineCoon, "Leo", "Big and fluffy", "https://cdn2.thecatapi.com/images/MTY3ODIyMQ.jpg"),
+                CatModel(Gender.Female, CatBreed.Persian, "Luna", "Elegant queen", "https://cdn2.thecatapi.com/images/0XYvRd7oD.jpg"),
+                CatModel(Gender.Male, CatBreed.Siamese, "Max", "Talkative friend", "https://cdn2.thecatapi.com/images/ai6.jpg"),
+                CatModel(Gender.Unknown, CatBreed.Ragdoll, "Snowy", "Always relaxed", "https://cdn2.thecatapi.com/images/m7v.jpg"),
+                CatModel(Gender.Female, CatBreed.Sphynx, "Nina", "Bold and unique", "https://cdn2.thecatapi.com/images/123.jpg"),
+                CatModel(Gender.Male, CatBreed.Bengal, "Rocky", "Energetic hunter", "https://cdn2.thecatapi.com/images/8li.jpg"),
+                CatModel(Gender.Unknown, CatBreed.Abyssinian, "Milo", "Curious explorer", "https://cdn2.thecatapi.com/images/ozEvzdVM-.jpg")
             )
         )
     }
--- app/src/main/java/com/example/lab_week_06/model/CatBreed.kt
+++ app/src/main/java/com/example/lab_week_06/model/CatBreed.kt
@@ -1,5 +1,14 @@
 package com.example.lab_week_06.model
 
 enum class CatBreed {
-    AmericanCurl, BalineseJavanese, ExoticShorthair
+    AmericanCurl,
+    BalineseJavanese,
+    ExoticShorthair,
+    MaineCoon,
+    Persian,
+    Siamese,
+    Ragdoll,
+    Sphynx,
+    Bengal,
+    Abyssinian
 }
