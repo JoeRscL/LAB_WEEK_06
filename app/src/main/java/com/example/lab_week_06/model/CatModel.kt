bc3b6f9f7ccbb5469aa9f0eed5e1a8218614b30b
JoeRscL <jonathan20strada@gmail.com>
2025-10-03 11:01:19 +0700

    Commit No.1 - Adding a RecyclerView and Populating it

---
+++ app/src/main/java/com/example/lab_week_06/model/CatModel.kt
@@ -0,0 +1,8 @@
+package com.example.lab_week_06.model
+data class CatModel(
+    val gender: Gender,
+    val breed: CatBreed,
+    val name: String,
+    val biography: String,
+    val imageUrl: String
+)
